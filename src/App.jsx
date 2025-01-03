0e131802c42e558292364dfba4c0e422c97f5437
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-01-03 15:10:09 +0530

    Added Csv Upload Form for bulk uploads from csv file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 
 import './App.css'
 import AddQuizData from './components/addQuestion'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CsvUploader from './components/CsvForm'
 
 function App() {
 
 
   return (
     <div className='bg-slate-500'>
-      <AddQuizData/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/add-manually' element={<AddQuizData />} />
+      <Route path='/add-csv' element={<CsvUploader />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
